a7520937d27cd7c85d74737442c2ed5045518d9d
unknown <cyberworld.kanal@gmail.com>
2024-04-20 12:27:37 +0200

    refreshing a MCDU screen

--- WebPanel/mcdu/mcdu.js
+++ WebPanel/mcdu/mcdu.js
@@ -1,6 +1,52 @@
-const screenshot_mcdu1 = "/screenshot?canvasindex=15&type=jpg"
+// Code inspired by: https://github.com/legoboyvdlp/A320-family/blob/dev/WebPanel/js/mcdu.js
 
-function rerender_img() 
-{
-    
-}
+const MCDU = (() => {
+    screenshot_mcdu1 = "/screenshot?canvasindex=8&type=jpg";
+    refresh_cooldown = 500;
+
+    function init()
+    {
+        console.info("MCDU INIT");
+
+        // H
+		setInterval(refreshMcduImg, refresh_cooldown);
+    }
+
+    function getMcduUrl(baseUrl)
+    {
+        cacheDate = new Date().getTime();
+        return new Promise((resolve, reject) => {
+            const new_url = screenshot_mcdu1 + '&cacheBust=' + cacheDate;
+            const check_img = new Image;
+
+            check_img.addEventListener('error', reject);
+
+            check_img.addEventListener('load', (event) => {
+                resolve(new_url);
+            });
+            check_img.src = new_url;
+        });
+    }
+
+    function mcduRefreshFail()
+    {
+        let err_msg = "Loading MCDU image, try refresh page";
+
+        console.error(err_msg);
+        alert(err_msg);
+    }
+
+    function setMcduUrl(url)
+    {
+        document.getElementById("mcduimg").src = url;
+    }
+
+    function refreshMcduImg() 
+    {
+        getMcduUrl()
+         .then(setMcduUrl)
+         .catch(mcduRefreshFail);        
+    }
+
+    init();
+})();
